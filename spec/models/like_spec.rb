ace16c31435fae00b90770644783404f80b01639
Alexey Rudenkov <108358127+alex1-11@users.noreply.github.com>
2023-04-19 09:41:12 +0300

    Made spec for Like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:like) }
+
+  describe 'association' do
+    it { should belong_to(:user) }
+    it { should belong_to(:post) }
+  end
+
+  describe 'validation' do
+    context 'with nil user data' do
+      before { subject.user_id = nil }
+      it { should be_invalid }
+    end
+
+    context 'with nil post data' do
+      before { subject.post_id = nil }
+      it { should be_invalid }
+    end
+  end
 end
