4ffeab0628205a420a8d55c2c73e1828116d314c
Alexey Rudenkov <108358127+alex1-11@users.noreply.github.com>
2023-05-07 16:08:43 +0300

    Made specs for comments views: `index` and `_comment`. Fixed `comments/index` view

---
+++ spec/views/comments/_comment.html.erb_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.describe 'comments/_comment', type: :view do
+  subject           { rendered }
+  let(:user)        { create :user }
+  let(:sample_post) { create :post }
+  let(:comment)     { create :comment, post: sample_post, user: }
+
+  before do
+    sign_in user
+    render partial: 'comments/comment', locals: { comment: }
+  end
+
+  it { should have_selector("div#comment_#{comment.id}") }
+  it { should have_selector('strong', text: user.name) }
+  it { should have_selector('p', text: comment.text) }
+
+  it do
+    should have_selector("span[title='#{comment.created_at.localtime.to_fs(:long)}']",
+                         text: time_ago_in_words(comment.created_at))
+  end
+
+  shared_examples 'deletable comment' do
+    it { should have_selector("form[action='#{post_comment_path(sample_post, comment)}'][method='post']") }
+    it { should have_selector("input[name='_method'][type='hidden'][value='delete']", visible: false) }
+    it do
+      should have_selector(
+        "button[type='submit']"\
+        "[data-turbo-confirm='Delete this comment?']"\
+        "[data-confirm='Delete this comment?']",
+        text: 'x'
+      )
+    end
+  end
+
+  context 'own comment under post' do
+    it_behaves_like 'deletable comment'
+  end
+
+  context 'comment under own post' do
+    let(:sample_post) { create(:post, user:) }
+    let(:comment)     { create(:comment, post: sample_post) }
+
+    it_behaves_like 'deletable comment'
+  end
+
+  context 'foreign comment under foreign post' do
+    let(:sample_post) { create(:post) }
+    let(:comment)     { create(:comment, post: sample_post) }
+
+    it { should_not have_selector("form[action='#{post_comment_path(sample_post, comment)}'][method='post']") }
+    it { should_not have_selector("input[name='_method'][type='hidden'][value='delete']", visible: false) }
+    it do
+      should_not have_selector(
+        "button[type='submit']"\
+        "[data-turbo-confirm='Delete this comment?']"\
+        "[data-confirm='Delete this comment?']",
+        text: 'x'
+      )
+    end
+  end
+end
--- spec/views/comments/index.html.erb_spec.rb
+++ spec/views/comments/index.html.erb_spec.rb
@@ -1,26 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "comments/index", type: :view do
-  before(:each) do
-    assign(:comments, [
-      Comment.create!(
-        text: "Text",
-        user: nil,
-        post: nil
-      ),
-      Comment.create!(
-        text: "Text",
-        user: nil,
-        post: nil
-      )
-    ])
+  subject                  { rendered }
+  let(:user)               { create :user }
+  let(:sample_post)        { create :post }
+  let(:comments)           { create_list(:comment, 3, post: sample_post) }
+  let(:new_comment_record) { build(:comment, user:, post_id: sample_post.id) }
+
+  before do
+    sign_in user
+    assign(:post, sample_post)
+    assign(:comments, comments)
+    assign(:new_comment, new_comment_record)
+    render
   end
 
-  it "renders a list of comments" do
-    render
-    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
-    assert_select cell_selector, text: Regexp.new("Text".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(nil.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(nil.to_s), count: 2
+  it { should have_link('Back to post', href: post_path(sample_post)) }
+
+  it "renders post's caption with author name and text" do
+    should have_selector('div#caption')
+    should have_selector('strong', text: sample_post.user.name)
+    should have_selector('p', text: sample_post.caption)
   end
+
+  it { should have_selector('div#comments') }
+  it { should render_template(partial: '_comment', count: 3) }
+  it { should render_template(partial: '_form', count: 1) }
 end
