f6283335fd4cf8c20900e00824953760da0605f9
Alexey Rudenkov <108358127+alex1-11@users.noreply.github.com>
2023-05-11 02:06:34 +0300

    Generated `Relation` scaffold with follower/followed `_id`s

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :relations
   devise_for :users
   # Defines the root path route ("/")
   root "home#index"
---
+++ spec/factories/relations.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :relation do
+    follower_id { "" }
+    followed_id { "" }
+  end
+end
