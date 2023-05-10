b8ba2731449c95b8f7a35890388df9b41294b81e
Alexey Rudenkov <108358127+alex1-11@users.noreply.github.com>
2023-05-11 02:39:50 +0300

    Setting associations for `User` and `Relation` models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   has_many :liked_posts, through: :likes, source: :post
   has_many :comments, dependent: :destroy
 
+  has_many :active_relations,  class_name: 'Relation',
+                               foreign_key: 'follower_id',
+                               dependent: :destroy
+  has_many :passive_relations, class_name: 'Relation',
+                               foreign_key: 'followed_id',
+                               dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/models/relation_spec.rb
+++ spec/models/relation_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Relation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user)     { create :user }
+  let(:user2)    { create :user }
+  let(:relation) { Relation.new(follower: user, followed: user2) }
+
+  it 'is a valid object' do
+    expect(relation).to be_valid
+    expect { relation.save }.to change(Relation, :count).by(1)
+  end
+
+  it 'can create active relation' do
+    relation.save
+    expect(user.active_relations.last).to eq(relation)
+  end
+
+  it 'has follow method to create active relation (follow a user)' do
+    user.follow(user2)
+    expect(Relation.last).to be_valid
+  end
 end
