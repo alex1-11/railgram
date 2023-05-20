e8d1ce7cd88bab7c9f2e93b864d542c0397951ed
Alexey Rudenkov <108358127+alex1-11@users.noreply.github.com>
2023-05-21 01:38:51 +0300

    Uncommented some specs, removed comment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,67 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # describe 'validations' do
-  #   subject { build(:user) }
+  describe 'validations' do
+    subject { build(:user) }
 
-  #   it 'is a valid object' do
-  #     expect(subject).to be_valid
-  #   end
+    it 'is a valid object' do
+      expect(subject).to be_valid
+    end
 
-  #   it 'is not valid without an email' do
-  #     subject.email = nil
-  #     expect(subject).not_to be_valid
-  #   end
+    it 'is not valid without an email' do
+      subject.email = nil
+      expect(subject).not_to be_valid
+    end
 
-  #   it 'is not valid without a name' do
-  #     subject.name = nil
-  #     expect(subject).not_to be_valid
-  #   end
+    it 'is not valid without a name' do
+      subject.name = nil
+      expect(subject).not_to be_valid
+    end
 
-  #   it 'is not valid without a password' do
-  #     subject.password = nil
-  #     expect(subject).not_to be_valid
-  #   end
+    it 'is not valid without a password' do
+      subject.password = nil
+      expect(subject).not_to be_valid
+    end
 
-  #   it 'is not valid with a duplicate email' do
-  #     create(:user, email: subject.email)
-  #     expect(subject).not_to be_valid
-  #   end
+    it 'is not valid with a duplicate email' do
+      create(:user, email: subject.email)
+      expect(subject).not_to be_valid
+    end
 
-  #   it 'is not valid with a duplicate name' do
-  #     create(:user, name: subject.name)
-  #     expect(subject).not_to be_valid
-  #   end
+    it 'is not valid with a duplicate name' do
+      create(:user, name: subject.name)
+      expect(subject).not_to be_valid
+    end
 
-  #   it 'is not valid with an email length less than 5 chars' do
-  #     subject.email = FFaker::Lorem.characters(4)
-  #     expect(subject).not_to be_valid
-  #   end
+    it 'is not valid with an email length less than 5 chars' do
+      subject.email = FFaker::Lorem.characters(4)
+      expect(subject).not_to be_valid
+    end
 
-  #   it 'is not valid with a name length less than 3 chars' do
-  #     subject.name = FFaker::Lorem.characters(2)
-  #     expect(subject).not_to be_valid
-  #   end
+    it 'is not valid with a name length less than 3 chars' do
+      subject.name = FFaker::Lorem.characters(2)
+      expect(subject).not_to be_valid
+    end
 
-  #   it 'is not valid with a name length more than 30 chars' do
-  #     subject.name = FFaker::Lorem.characters(31)
-  #     expect(subject).not_to be_valid
-  #   end
+    it 'is not valid with a name length more than 30 chars' do
+      subject.name = FFaker::Lorem.characters(31)
+      expect(subject).not_to be_valid
+    end
 
-  #   it 'is not valid with a password length less than 6 chars' do
-  #     subject.password = FFaker::Lorem.characters(5)
-  #     expect(subject).not_to be_valid
-  #   end
-  # end
+    it 'is not valid with a password length less than 6 chars' do
+      subject.password = FFaker::Lorem.characters(5)
+      expect(subject).not_to be_valid
+    end
+  end
 
   describe 'associations' do
-    # it { should have_many(:posts) }
-    # it { should have_many(:likes) }
-    # it { should have_many(:liked_posts) }
-    # it { should have_many(:comments) }
-    # it { should have_many(:active_relations) }
-    # it { should have_many(:passive_relations) }
-    # it { should have_many(:following) }
+    it { should have_many(:posts) }
+    it { should have_many(:likes) }
+    it { should have_many(:liked_posts) }
+    it { should have_many(:comments) }
+    it { should have_many(:active_relations) }
+    it { should have_many(:passive_relations) }
+    it { should have_many(:following) }
 
     describe 'relations' do
       let(:user)     { create :user }
@@ -101,5 +101,3 @@ RSpec.describe User, type: :model do
     end
   end
 end
-
-# TODO: https://github.com/heartcombo/devise/wiki/How-To:-Test-controllers-with-Rails-(and-RSpec)
