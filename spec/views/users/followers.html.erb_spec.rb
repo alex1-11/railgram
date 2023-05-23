fed579e3327414c0f3377d54a8aa6de853de1fd2
Alexey Rudenkov <108358127+alex1-11@users.noreply.github.com>
2023-05-23 19:22:30 +0300

    Working on spec for `users/followers` view

---
+++ spec/views/users/followers.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'users/followers', type: :view do
+  subject            { rendered }
+  let(:user)         { create :user }
+  let(:sample_posts) { create_list(:post, 5, user:) }
+
+  before do
+    sign_in user
+    assign(:posts, sample_posts)
+    assign(:user, user)
+    render
+  end
+
+  it { should have_link('New post', href: new_post_path) }
+
+  it 'renders "users/_profile" partial for requested user' do
+    expect(view).to render_template(partial: 'users/_profile', count: 1)
+  end
+
+  it 'renders all the posts using "_post" partial' do
+    expect(view).to render_template(partial: '_post', count: 5)
+  end
+
+  it { should have_link('Show this post', href: post_path(sample_posts[0])) }
+  it { should have_link('Show this post', href: post_path(sample_posts[4])) }
+end
