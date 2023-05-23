041ec7b80f510178d8d599c713c4963eae1ca533
Alexey Rudenkov <108358127+alex1-11@users.noreply.github.com>
2023-05-23 18:27:46 +0300

    Fix to `shared/_navbar` partial spec

--- spec/views/shared/_navbar.html.erb_spec.rb
+++ spec/views/shared/_navbar.html.erb_spec.rb
@@ -14,7 +14,10 @@ RSpec.describe 'shared/_navbar', type: :view do
       expect(view).to render_template(partial: '_navbar', count: 1)
     end
 
-    it { should have_link('Feed', href: user_posts_path(user)) }
+    it { should_not have_link('Sign in', href: new_user_session_path) }
+    it { should_not have_link('Register', href: new_user_registration_path) }
+
+    it { should have_link('Feed', href: feed_path) }
     it { should have_link(user.name, href: user_path(user)) }
     it { should have_link('Settings', href: user_settings_path) }
     it 'have link to destroy user session with turbo attribute' do
@@ -32,5 +35,14 @@ RSpec.describe 'shared/_navbar', type: :view do
 
     it { should have_link('Sign in', href: new_user_session_path) }
     it { should have_link('Register', href: new_user_registration_path) }
+
+    it { should_not have_link('Feed', href: feed_path) }
+    it { should_not have_link(user.name, href: user_path(user)) }
+    it { should_not have_link('Settings', href: user_settings_path) }
+    it 'does not have link to destroy user session with turbo attribute' do
+      should_not have_link('Log out', href: destroy_user_session_path)
+      should_not have_selector("a[data-turbo-method='delete'][href='#{destroy_user_session_path}']")
+    end
+
   end
 end
