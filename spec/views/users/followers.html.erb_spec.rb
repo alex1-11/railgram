11d178c8b68c54d0f58a0700a0c737008559b9a8
Alexey Rudenkov <108358127+alex1-11@users.noreply.github.com>
2023-05-24 17:52:56 +0300

    Finished spec for `views/users/followers`. Changed <p> to <ul> with <li>s in view.

--- spec/views/users/followers.html.erb_spec.rb
+++ spec/views/users/followers.html.erb_spec.rb
@@ -1,27 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'users/followers', type: :view do
-  subject            { rendered }
-  let(:user)         { create :user }
-  let(:sample_posts) { create_list(:post, 5, user:) }
+  subject         { rendered }
+  let(:user)      { create :user }
+  let(:relations) { create_list(:relation, 3, followed: user) }
 
   before do
     sign_in user
-    assign(:posts, sample_posts)
-    assign(:user, user)
+    assign(:followers, relations.map(&:follower))
     render
   end
 
-  it { should have_link('New post', href: new_post_path) }
+  it { should have_selector('div#followers') }
 
-  it 'renders "users/_profile" partial for requested user' do
-    expect(view).to render_template(partial: 'users/_profile', count: 1)
+  it 'has list of links to all followers' do
+    should have_selector('ul')
+    should have_link(relations[0].follower.name, href: user_path(relations[0].follower))
+    should have_link(relations[1].follower.name, href: user_path(relations[1].follower))
+    should have_link(relations[2].follower.name, href: user_path(relations[2].follower))
   end
-
-  it 'renders all the posts using "_post" partial' do
-    expect(view).to render_template(partial: '_post', count: 5)
-  end
-
-  it { should have_link('Show this post', href: post_path(sample_posts[0])) }
-  it { should have_link('Show this post', href: post_path(sample_posts[4])) }
 end
