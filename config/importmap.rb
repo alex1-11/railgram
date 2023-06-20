6e0ebc76328721374ba7e285b8ee9ff817a78272
Alexey Rudenkov <108358127+alex1-11@users.noreply.github.com>
2023-06-20 19:05:47 +0300

    Solved issue with BS JS not being loaded to app

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,9 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# Big thanks to @coorasse for reply and blogpost: 
+# https://github.com/twbs/bootstrap-rubygem/issues/228#issuecomment-1011407591
+# https://dev.to/coorasse/rails-7-bootstrap-5-and-importmaps-without-nodejs-4g8
+pin "popper", to: "popper.js", preload: true 
 pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "popper.js", preload: true
