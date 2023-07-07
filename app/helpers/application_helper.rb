0b3e2fc8cb0a4c9e2d810b6555ac0b5eed9f8724
Alexey Rudenkov <108358127+alex1-11@users.noreply.github.com>
2023-07-07 19:03:56 +0300

    Fixed colors for navbar to be unreliable from page theme (light or dark)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def tag_nav_link(text, path)
-    link_classes = 'nav-link'
-    link_classes += ' active bold' if current_page?(path)
+    link_classes = 'nav-link text-dark'
+    link_classes += ' fw-bold' if current_page?(path)
     tag.li(link_to(text, path, class: link_classes), class: 'nav-item')
   end
 end
