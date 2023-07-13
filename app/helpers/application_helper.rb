8dbca23e0187979701e8153e238003970bbcd7e6
Alexey Rudenkov <108358127+alex1-11@users.noreply.github.com>
2023-07-13 14:03:07 +0300

    changed colors for navbar to fix darktheme switching

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def tag_nav_link(text, path)
-    link_classes = 'nav-link text-dark'
+    link_classes = 'nav-link'
     link_classes += ' fw-bold' if current_page?(path)
     tag.li(link_to(text, path, class: link_classes), class: 'nav-item')
   end
