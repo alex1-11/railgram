4b3bb33da8d7c81d2070392b2f6121058ca84b69
Alexey Rudenkov <108358127+alex1-11@users.noreply.github.com>
2023-08-01 23:48:32 +0300

    Fixed specs for `posts/_form, new` views

--- spec/views/posts/_form.html.erb_spec.rb
+++ spec/views/posts/_form.html.erb_spec.rb
@@ -11,11 +11,11 @@ RSpec.describe 'posts/_form', type: :view do
   end
 
   it { should have_selector('input#post_image[name="post[image]"][type="file"]') }
-  it { should have_selector('label[for="post_caption"]', text: 'Caption') }
+  it { should have_selector('span', text: 'Caption') }
 
   context 'creating new post' do
     it { should have_selector("form[action='#{url_for(sample_post)}'][method='post']") }
-    it { should have_selector("input[name='post[caption]'][type='text']") }
+    it { should have_selector("textarea#post_caption[name='post[caption]']") }
     it { should have_selector('input[type="submit"][value="Create Post"]') }
   end
 
@@ -24,7 +24,7 @@ RSpec.describe 'posts/_form', type: :view do
 
     it { should have_selector("form[action='#{post_path(sample_post)}'][method='post']") }
     it { should have_selector('input[name="_method"][type="hidden"][value="patch"]', visible: false) }
-    it { should have_selector("input[name='post[caption]'][type='text'][value='#{sample_post.caption}']") }
+    it { should have_selector("textarea[name='post[caption]']", text: sample_post.caption) }
     it { should have_selector('input[type="submit"][value="Update Post"]') }
   end
 
@@ -36,7 +36,7 @@ RSpec.describe 'posts/_form', type: :view do
     end
 
     it 'displays the error messages' do
-      expect(subject).to have_selector('div[class="alert alert-danger"]')
+      expect(subject).to have_selector('div[class="alert alert-danger alert-dismissible fade show text-start"]')
       expect(subject).to have_selector('h2', text: '2 errors prohibited this post from being saved:')
       expect(subject).to have_selector('ul')
       expect(subject).to have_selector('li', text: "Caption can't be blank")
